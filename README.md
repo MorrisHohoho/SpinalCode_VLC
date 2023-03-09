eef4d7254b93013b44d27ec6f8f2e7299abcaed8
MorrisHohoho <morrishohoho@qq.com>
2023-03-09 19:26:02 +0800

    Demo V2, not bad

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # SpinalCode_C
 SpinalCode implementation in C.
 # Warning
-Still in Development
+**Memory Leak!!!!!**
 # How to use
 1. ```mkdir build```
 2. ```cd build```
@@ -10,5 +10,8 @@ Still in Development
 
 
 # Note
-If the message is consisted of all numbers(in ascii), hash collision is serve.
-But if we put some letters between the numbers, the probability of decoding right is high.
+1. If the message is consisted of all numbers(in ascii), hash collision is serve.But if we put some letters between the numbers, the probability of decoding correctly is high.
+
+2. If change k, the definition ```CHILD_NUMS``` in ```multinodetree.h``` should been changed too.
+
+3. The implementation of multinodetree is to use ```malloc```, but the memory haven't been ```free``` when the ```SpinalDecode``` function is over.
--- src/include/decoder/decoder.h
+++ src/include/decoder/decoder.h
@@ -1,25 +1,37 @@
 #ifndef DECODER_H
 #define DECODER_H
 #define EXPAND_DEPTH 1
+#include<stdint.h>
 #include "multinodetree/multinodetree.h"
 #include"vector/vector.h"
+
 struct Candidate
 {
     int cost;
-    struct MultiTree *tree;
-    struct Candidate *next;
+    struct MultiTree *tree;     //The address of tree node.
+    struct Candidate *next;     //The address of next candidate.
 };
 
+//Add candidate node to candidates vector.
+void add_candidates(struct Candidate *head, struct Candidate *NEX);
 
 
-void Symbols2Int(const  char *symbols, int *res,int len,int c);
-
-
-void SpinalDecode(const char* symbols,const int symbols_packet_len,char * decoded_message,int message_len,int k,int c,int B);
-
+//Sorting candidates in terms of cost from lowest to highest.(Using quick_sort algorithm)
 void sorting_candidates(vector* candidates,int l,int r);
 
-void add_candidates(struct Candidate *head, struct Candidate *NEX);
+
+//Recover symbols from seperated byte flow.
+void Symbols2Int(const  char *symbols, uint32_t *res,int len);
+
+
+//Recover message from seperated k bits.
+void getDecodedMessage(struct MultiTree *node, char *decoded_message, int len);
+
+/*
+ * SpinalCode decode
+ */
+void SpinalDecode(const char* symbols,const int symbols_packet_len,char * decoded_message,int message_len);
+
 
 
 #endif
