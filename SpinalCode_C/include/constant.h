141cf254496cf75b775e8fdbb3e89e77cf74c1e0
morrishohoho <morrishohoho@qq.com>
2023-07-16 15:50:56 +0800

    fix d bug

--- SpinalCode_C/include/constant.h
+++ SpinalCode_C/include/constant.h
@@ -10,12 +10,18 @@
 //Spinalcodes parameters
 #define K 3 // no more than 8
 #define C 6  //MAX C is 8 because of the size of uint8_t
-#define B 4  
-#define D 5 // if D > 5, the decoding latency is up to 12s.
+#define B 4
+#define D 1 // if D > 5, the decoding latency is up to 12s.
 #define PASS 3
 
+#if (D==1)
+#define WAVEFRONT_MAX B*(1<<(K*(D)))*(1<<(K*(D)))
+#define SUBTREES_MAX B*(1<<(K*(D)))
+#else
 #define WAVEFRONT_MAX B*(1<<(K*(D-1)))*(1<<(K*(D-1)))
 #define SUBTREES_MAX B*(1<<(K*(D-1)))
+#endif
+
 #define SPINE_LENGTH ((8*MES_LENGTH+K-1)/K)
 #define PASS_LENGTH (SPINE_LENGTH*PASS)
 
